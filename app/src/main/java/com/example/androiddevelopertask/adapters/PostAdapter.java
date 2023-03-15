dc6cbd69fb4e021e1d3d55e4e53a0b58447db0c4
mehedi_hassan <mehedii.hassan01@gmail.com>
2023-03-15 14:51:34 +0600

    Post details page design done

--- app/src/main/java/com/example/androiddevelopertask/adapters/PostAdapter.java
+++ app/src/main/java/com/example/androiddevelopertask/adapters/PostAdapter.java
@@ -1,11 +1,14 @@
 package com.example.androiddevelopertask.adapters;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androiddevelopertask.R;
 import com.example.androiddevelopertask.databinding.RvRowItemDesignBinding;
 import com.example.androiddevelopertask.models.PostResponseModel;
 
@@ -34,6 +37,12 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
 
         holder.binding.tvTitle.setText(postList.get(position).getTitle());
         holder.binding.tvDetails.setText(postList.get(position).getBody());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.postDetailsPageFragment);
+            }
+        });
     }
 
     public void submitNewList(List<PostResponseModel> postList) {
